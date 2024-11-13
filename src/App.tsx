8ec1562e4652b0ed39cae4f1efa2a6270c7a3d83
Bruno Cardoso <brunocardoso.developer@gmail.com>
2024-11-13 09:00:13 -0300

    crate project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">This is my weather!</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  devServer: {
+    port: 3004,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "weather",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Weather": "./src/App",
+      },
+      shared: {
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: require("./package.json").dependencies.react,
+        },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: require("./package.json").dependencies["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "http://localhost:3004/",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  target: "web",
+};
